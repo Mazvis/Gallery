c746bb1ac966561062ff4c69daeb4b05a0f360f5
Ernestas <Ernestas@Ernesto>
2014-05-20 22:36:01 +0300

    Panaikinti LoginController.php ir RegistrationController.php, jų funkcionalumai sudėti į UserController.php

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,8 +1,65 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Class UserController
  */
+class UserController extends BaseController {
 
+    /**
+     * Store registration form data to variable
+     *
+     * @return message
+     */
+    public function storeGet() {
+        $user = new User;
+        $input = Input::all();
+        return $user->validateField($input);
+    }
+
+    /**
+     * Sign in page
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function tryLogin() {
+        if (Auth::attempt(array('username' => Input::get('username'), 'password' => Input::get('password')))) {
+            //return Redirect::intended('/');
+            return Redirect::back();
+        }else{
+            return Redirect::to('/login')->with('tried_login', Input::get('username'));
+        }
+    }
+
+    /**
+     * log
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function userLogout() {
+        Auth::logout();
+        return Redirect::intended('/');
+    }
+    
+    /**
+     * Authentificates user with Auth::attempt command
+     *
+     * @return \Illuminate\Http\RedirectResponse if success redirects to main page else redirects with errors
+     */
+    public function authLogin() {
+        if (Auth::attempt(array('username' => Input::get('username'), 'password' => Input::get('password')))) {
+            return Redirect::intended('/');
+        }else{
+            return Redirect::to('/')->with('tried_login', Input::get('username'));
+        }
+    }
+
+    /**
+     * Makes logout
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout() {
+        $logout = new Login;
+        return $logout->logout();
+    }
+}
